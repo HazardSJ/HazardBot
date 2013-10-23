9b6ea92269545195f0ccc44d35e612a9e4b8e36e
Hazard-SJ <hazardsjwiki@gmail.com>
2013-10-22 20:03:37 -0500

    Added another link to summary

--- commonswiki/internationalization.py
+++ commonswiki/internationalization.py
@@ -129,7 +129,10 @@ class InternationalizationBot(object):
             if self.makeFixes(text):
                 pywikibot.showDiff(text, unicode(self.code))
                 try:
-                    page.put(unicode(self.code), comment="[[Commons:Bots|Bot]]: Applied fixes for internationalization support")
+                    page.put(
+                        unicode(self.code),
+                        comment="[[Commons:Bots|Bot]]: Applied fixes for [[Commons:Template i18n|internationalization support]]"
+                    )
                 except (Exception, pywikibot.Error), error:
                     print "\nError: %s\n" % error
 
