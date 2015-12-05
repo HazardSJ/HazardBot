668fcd662200dcedd15ef6408b2b2f9119c25796
Hazard-SJ <hazardsjwiki@gmail.com>
2015-12-04 21:35:40 -0500

    added another category

---
+++ enwiki/afd_relisted_cat_remover.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Published by Hazard-SJ (https://wikitech.wikimedia.org/wiki/User:Hazard-SJ)
+# under the terms of Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+# https://creativecommons.org/licenses/by-sa/3.0/
+
+import re
+import pywikibot
+
+pywikibot.config.family = "wikipedia"
+pywikibot.config.mylang = "en"
+site = pywikibot.Site()
+site.login()
+
+
+class AFDRelistedCatRemover(object):
+    """Removes closed from relisted AfD categories"""
+
+    def __init__(self):
+        self.categories = [
+            pywikibot.Category(site, "Category:AfD debates relisted 3 or more times"),
+            pywikibot.Category(site, "Category:Relisted AfD debates")
+        ]
+
+    def page_generator(self, category):
+        for page in category.articles():
+            if not page.title().startswith("Wikipedia:Articles for deletion/"):
+                continue
+            if page.title().startswith("Wikipedia:Articles for deletion/Log/"):
+                continue
+            if "xfd-closed" not in page.get():
+                continue
+            yield page
+
+    def run(self):
+        for category in self.categories:
+            pywikibot.output(category.title(asLink=True) + "\n")
+
+            for page in self.page_generator(category):
+                try:
+                    pywikibot.output(page.title(asLink=True))
+                except UnicodeEncodeError:
+                    pass
+
+                try:
+                    text = page.get()
+                except pywikibot.Error:
+                    continue
+
+                categories = re.findall("(?:<noinclude>)?\[\[%s(?:\|.*?)?\]\](?:</noinclude>)?" % category.title(), text)
+                if not categories:
+                    continue
+
+                text = text.replace(categories[0], "")
+
+                try:
+                    page.put(text, "[[Wikipedia:Bots|Bot]]: Removing closed AfD from %s" % category.title(asLink=True))
+                except pywikibot.Error:
+                    pywikibot.output("Could not remove the category from %s" % page.title())
+
+
+if __name__ == "__main__":
+    try:
+        bot = AFDRelistedCatRemover()
+        bot.run()
+    finally:
+        pywikibot.stopme()
