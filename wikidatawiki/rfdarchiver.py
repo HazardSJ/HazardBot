9253c75005c6a07b0898c431bc4b47cb03f41fdb
Hazard-SJ <hazardsjwiki@gmail.com>
2015-12-05 18:22:48 -0500

    primarily PEP8 fixes, but should also work on PY3 now

--- wikidatawiki/rfdarchiver.py
+++ wikidatawiki/rfdarchiver.py
@@ -22,30 +22,30 @@ site.login()
 
 class RFDArchiverBot(object):
     def __init__(self):
-        self.timeDiff = float(0.5 * (60 * 60))
-        self.currentTime = time.gmtime()
-        self.currentTimestamp = time.mktime(self.currentTime)
-        rfdTitle = "Wikidata:Requests for deletions"
-        archiveTitle = "%s/Archive/%s" % (rfdTitle, time.strftime("%Y/%m/%d", self.currentTime))
-        self.rfdPage = pywikibot.Page(site, rfdTitle)
-        self.archivePage = pywikibot.Page(site, archiveTitle)
-        self.archiveHeader = "{{Archive|category=Archived requests for deletion}}"
-        self.responseTemplates = ["done", "deleted", "not done", "not deleted", "didn't delete"]
-        self.archiveCount = 0
+        self.time_diff = float(0.5 * (60 * 60))
+        self.current_time = time.gmtime()
+        self.current_timestamp = time.mktime(self.current_time)
+        rfd_title = "Wikidata:Requests for deletions"
+        archive_title = "%s/Archive/%s" % (rfd_title, time.strftime("%Y/%m/%d", self.current_time))
+        self.rfd_page = pywikibot.Page(site, rfd_title)
+        self.archive_page = pywikibot.Page(site, archive_title)
+        self.archive_header = "{{Archive|category=Archived requests for deletion}}"
+        self.response_templates = ["done", "deleted", "not done", "not deleted", "didn't delete"]
+        self.archive_count = 0
 
     def processRequests(self):
-        sections = self.rfdCode.get_sections(levels=[2])
+        sections = self.rfd_code.get_sections(levels=[2])
         for section in sections:
             responded = False
             templates = [template.name.lower().strip() for template in section.ifilter_templates()]
-            for responseTemplate in self.responseTemplates:
+            for responseTemplate in self.response_templates:
                 if responseTemplate in templates:
                     responded = True
                     break
             if not responded:
                 continue
             timestamps = re.findall(
-                "\d{1,2}:\d{2},\s\d{1,2}\s\D{3,9}\s\d{4}\s\(UTC\)", unicode(section), re.DOTALL
+                "\d{1,2}:\d{2},\s\d{1,2}\s\D{3,9}\s\d{4}\s\(UTC\)", section.__unicode__(), re.DOTALL
             )
             timestamps = sorted(
                 [time.mktime(time.strptime(timestamp[:-6], "%H:%M, %d %B %Y")) for timestamp in timestamps]
@@ -54,23 +54,23 @@ class RFDArchiverBot(object):
                 ts = timestamps[-1]
             except IndexError:
                 continue
-            if (self.currentTimestamp - ts) > self.timeDiff:
-                self.archiveCode.append(section)
+            if (self.current_timestamp - ts) > self.time_diff:
+                self.archive_code.append(section)
                 while section:
-                    self.rfdCode.remove(section.get(0))
-                self.archiveCount += 1
+                    self.rfd_code.remove(section.get(0))
+                self.archive_count += 1
 
-    def getSummary(self, isArchive = False):
+    def get_summary(self, is_archive=False):
         summary = "[[Wikidata:Bots|Bot]]: %s %i request%s %s %s" % (
-            "Archived" if isArchive else "Archiving",
-            self.archiveCount,
-            "" if self.archiveCount == 1 else "s",
-            "from" if isArchive else "to",
-            self.rfdPage.title(allowInterwiki = False, asLink = True) if isArchive else self.archivePage.title(allowInterwiki = False, asLink = True)
+            "Archived" if is_archive else "Archiving",
+            self.archive_count,
+            "" if self.archive_count == 1 else "s",
+            "from" if is_archive else "to",
+            self.rfd_page.title(asLink=True) if is_archive else self.archive_page.title(asLink=True)
         )
-        if isArchive:
+        if is_archive:
             return summary
-        version = self.rfdPage.getVersionHistory(total=1)[0]
+        version = self.rfd_page.getVersionHistory(total=1)[0]
         user = version[2]
         params = {
             "action": "query",
@@ -78,30 +78,30 @@ class RFDArchiverBot(object):
             "ususers": user,
             "usprop": "groups"
         }
-        userGroups = Request(**params).submit()["query"]["users"][0]["groups"]
+        user_groups = Request(**params).submit()["query"]["users"][0]["groups"]
         summary += " (last edit at %s by [[User:%s|]]%s%s" % (
             version[1],
             user,
-            " (administrator)" if (not ("bot" in userGroups) and ("sysop" in userGroups)) else "",
+            " (administrator)" if (not ("bot" in user_groups) and ("sysop" in user_groups)) else "",
             ": '%s'" % version[3] if version[3] else ""
         )
         return summary
 
     def run(self):
-        Request(action="purge", titles=self.rfdPage.title()).submit()
-        rfdText = self.rfdPage.get()
-        if self.archivePage.exists():
-            archiveText = self.archivePage.get()
+        rfd_text = self.rfd_page.get(force=True)
+        if self.archive_page.exists():
+            archive_text = self.archive_page.get()
         else:
-            archiveText = self.archiveHeader
-        self.rfdCode = mwparserfromhell.parser.Parser().parse(rfdText, skip_style_tags=True)
-        self.archiveCode = mwparserfromhell.parser.Parser().parse(archiveText + "\n\n", skip_style_tags=True)
+            archive_text = self.archive_header
+        self.rfd_code = mwparserfromhell.parser.Parser().parse(rfd_text, skip_style_tags=True)
+        self.archive_code = mwparserfromhell.parser.Parser().parse(archive_text + "\n\n", skip_style_tags=True)
         self.processRequests()
-        if not self.archiveCount:
-            print "There are no archivable requests."
+        if not self.archive_count:
+            pywikibot.output("There are no archivable requests.")
             return
-        self.rfdPage.put(unicode(self.rfdCode), comment = self.getSummary())
-        self.archivePage.put(unicode(self.archiveCode), comment = self.getSummary(isArchive = True))
+        self.rfd_page.put(self.rfd_code, comment=self.get_summary())
+        self.archive_page.put(self.archive_code, comment=self.get_summary(is_archive=True))
+
 
 def main():
     bot = RFDArchiverBot()
