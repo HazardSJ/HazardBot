384e8697902ca27bff2a80737f04c3d0635a720c
Hazard-SJ <hazardsjwiki@gmail.com>
2015-12-05 18:11:33 -0500

    cleaned up code, and added support for {{notdone}}

--- wikidatawiki/rforarchiver.py
+++ wikidatawiki/rforarchiver.py
@@ -20,23 +20,24 @@ site.login()
 
 
 class RFORArchiverBot(object):
-    def __init__(self):
-        self.basePage = pywikibot.Page(site, "Wikidata:Requests for permissions")
-        self.requestsPage = pywikibot.Page(site, self.basePage.title() + "/Other rights")
-        self.archiveTitles = {
-            "confirmed": self.basePage.title() + "/RfConfirmed/%s",
-            # "ipblock-exempt": self.basePage.title() + "",  # Not archived currently
-            "propertycreator": self.basePage.title() + "/RfPropertyCreator/%s",
-            "rollbacker": self.basePage.title() + "/RfRollback/%s"
-        }
-        self.closedRegex = "\n\{\{\s*(?:A(?:rchive)?|D(?:iscussion)?) ?top\|.*?\}\}.*?"
-        self.closedRegex += "\{\{\s*(?:A(?:rchive)?|D(?:iscussion)?) ?bottom\}\}\n"
-        self.archiveText = "{{Archive|category=Archived requests for permissions}}"
-        self.archiveText += "\n__TOC__\n{{Discussion top}}"
+    """Archives closed requests on [[Wikidata:Requests for permissions/Other rights]]"""
 
-        
+    def __init__(self):
+        self.base_page = pywikibot.Page(site, "Wikidata:Requests for permissions")
+        self.requests_page = pywikibot.Page(site, self.base_page.title() + "/Other rights")
+        self.archive_titles = {
+            "confirmed": self.base_page.title() + "/RfConfirmed/%s",
+            # "ipblock-exempt": self.basePage.title() + "",  # Not archived currently
+            "propertycreator": self.base_page.title() + "/RfPropertyCreator/%s",
+            "rollbacker": self.base_page.title() + "/RfRollback/%s"
+        }
+        self.closed_regex = "\n\{\{\s*(?:A(?:rchive)?|D(?:iscussion)?) ?top\|.*?\}\}.*?"
+        self.closed_regex += "\{\{\s*(?:A(?:rchive)?|D(?:iscussion)?) ?bottom\}\}\n"
+        self.archive_text = "{{Archive|category=Archived requests for permissions}}"
+        self.archive_text += "\n__TOC__\n{{Discussion top}}"
+
     def run(self):
-        text = self.requestsPage.get()
+        text = self.requests_page.get()
         code = mwparserfromhell.parser.Parser().parse(text, skip_style_tags=True)
         for section in code.get_sections(levels=[2]):
             if "confirmed" in section.filter_headings()[0].title.lower():
@@ -50,10 +51,10 @@ class RFORArchiverBot(object):
             archivable = list()
             for discussion in section.get_sections(levels=[4]):
                 templates = [template.name.lower().strip() for template in discussion.ifilter_templates()]
-                if not ("done" in templates or "not done" in templates):
+                if not ("done" in templates or "not done" in templates or "notdone" in templates):
                     continue
                 timestamps = re.findall(
-                    "\d{1,2}:\d{2},\s\d{1,2}\s\D{3,9}\s\d{4}\s\(UTC\)", unicode(discussion)
+                    "\d{1,2}:\d{2},\s\d{1,2}\s\D{3,9}\s\d{4}\s\(UTC\)", discussion.__unicode__()
                 )
                 timestamps = sorted(
                     [datetime.strptime(timestamp[:-6], "%H:%M, %d %B %Y") for timestamp in timestamps]
@@ -64,27 +65,27 @@ class RFORArchiverBot(object):
                 continue
             archive = pywikibot.Page(
                 site,
-                self.archiveTitles[group] % datetime.utcnow().strftime("%B %Y")
+                self.archive_titles[group] % datetime.utcnow().strftime("%B %Y")
             )
             if archive.exists():
-                archiveText = archive.get()
+                archive_text = archive.get()
             else:
-                archiveText = self.archiveText
-                archiveText += "\n\n"
-                archiveCode = mwparserfromhell.parse(archiveText)
+                archive_text = self.archive_text
+                archive_text += "\n\n"
+            archive_code = mwparserfromhell.parse(archive_text)
             for add in archivable:
-                append = unicode(add).strip()
-                if not append in unicode(archiveCode):
-                    archiveCode.append(append + "\n\n")
+                append = add.strip()
+                if append not in archive_code:
+                    archive_code.append(append + "\n\n")
             for remove in archivable:
                 code.remove(remove)
             archive.put(
-                unicode(archiveCode),
-                "[[Wikidata:Bots|Bot]]: Archiving from %s" % self.requestsPage.title(asLink=True)
+                archive_code,
+                "[[Wikidata:Bots|Bot]]: Archiving from %s" % self.requests_page.title(asLink=True)
             )
-        pywikibot.showDiff(text, unicode(code))
-        if text != unicode(code):
-            self.requestsPage.put(unicode(code), "[[Wikidata:Bots|Bot]]: Archived closed requests")
+        pywikibot.showDiff(text, code)
+        if text != code:
+            self.requests_page.put(code, "[[Wikidata:Bots|Bot]]: Archived closed requests")
 
 
 def main():
