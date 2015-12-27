0808886448b317429297803ed322da58acfa0de8
Hazard-SJ <hazardsjwiki@gmail.com>
2015-12-27 17:09:07 -0500

    SubstitutionBot now in one file

--- multiple/substituter.py
+++ multiple/substituter.py
@@ -5,6 +5,7 @@
 
 
 from __future__ import unicode_literals
+import sys
 
 import mwparserfromhell
 import pywikibot
@@ -14,10 +15,10 @@ from pywikibot import pagegenerators
 class SubstitutionBot(object):
     """Finds templates that should be substituted and substitutes them as necessary"""
 
-    def __init__(self, site, template):
+    def __init__(self, site, template_name):
         self.site = site
         self.site.login()
-        self.source_template = template
+        self.source_template = pywikibot.Page(site, template_name)
 
     def _templates_generator(self):
         generator = pagegenerators.NamespaceFilterPageGenerator(
@@ -46,15 +47,46 @@ class SubstitutionBot(object):
                 for temp in code.ifilter_templates():
                     if temp.has_param("nosubst") or temp.has_param("demo"):
                         continue
+                    replace = False
                     if temp.name.lower().strip() == title:
+                        replace = True
+                    if temp.name.lower().strip().startswith("template:") and temp.name.lower().strip()[9:] == title:
+                        replace = True
+                    if replace:
                         temp.name = "subst:%s" % template.title()
                         temp.add("subst", "subst:")
                 if text != code:
                     pywikibot.showDiff(text, code)
-                    try:
-                        page.put(
-                            code,
-                            "Bot: Substituting {{%s}}" % template.title(asLink=True, allowInterwiki=False)
-                        )
-                    except pywikibot.Error:
-                        continue
+                    # try:
+                    #     page.put(
+                    #         code,
+                    #         "Bot: Substituting {{%s}}" % template.title(asLink=True, allowInterwiki=False)
+                    #     )
+                    # except pywikibot.Error:
+                    #     continue
+
+
+def main():
+    site = None
+    if len(sys.argv) > 1:
+        db_name = sys.argv[1]
+        if db_name == "commonswiki":
+            site = pywikibot.Site("commons", "commons")
+            template_name = "Template:Must be substituted"
+        # elif db_name == "enwiki":
+        #     site = pywikibot.Site("en", "wikipedia")
+        #     template_name = "Template:Subst only"
+        elif db_name == "wikidatawiki":
+            site = pywikibot.Site("wikidata", "wikidata")
+            template_name = "Template:Subst only"
+    if site is None:
+        raise Exception("Please specify a valid database name.")
+    else:
+        bot = SubstitutionBot(site, template_name)
+        bot.run()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
