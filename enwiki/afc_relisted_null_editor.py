dc35235a2cbc2e86e21df291ad31353b03a73b10
Hazard-SJ <hazardsjwiki@gmail.com>
2016-02-03 01:04:03 -0500

    null edit bot to aid relisted AfCs task per my enwiki talk

---
+++ enwiki/afc_relisted_null_editor.py
@@ -0,0 +1,35 @@
+import pywikibot
+
+pywikibot.config.family = "wikipedia"
+pywikibot.config.mylang = "en"
+site = pywikibot.Site()
+site.login()
+
+
+class AFDRelistedNullEditor(object):
+    """Makes a null edit to AfDs transcluded in the logs on [[Category:Relisted AfD debates]]"""
+
+    def __init__(self):
+        self.afd_prefix = "Wikipedia:Articles for deletion/"
+        self.category = pywikibot.Category(site, "Category:Relisted AfD debates")
+
+    def process_log(self, log):
+        for page in log.itertemplates():
+            if page.title().startswith(self.afd_prefix):
+                try:
+                    page.touch()
+                except pywikibot.Error:
+                    pass
+
+    def run(self):
+        for page in self.category.articles():
+            if page.title().startswith(self.afd_prefix + "Log/"):
+                self.process_log(page)
+
+
+if __name__ == "__main__":
+    try:
+        bot = AFDRelistedNullEditor()
+        bot.run()
+    finally:
+        pywikibot.stopme()
