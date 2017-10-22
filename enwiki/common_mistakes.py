8ebeaf075c7933648b4fbb713da2344ee8a5a4c0
Hazard-SJ <hazardsjwiki@gmail.com>
2017-10-22 12:48:45 -0400

    Allow enwiki WP:FIX to auto-determine dump file

--- enwiki/common_mistakes.py
+++ enwiki/common_mistakes.py
@@ -13,12 +13,25 @@ site.login()
 
 class CommonMistakesLister(object):
     """Updates lists of common mistakes for WikiProject Fix common mistakes"""
+    
+    DUMP_PATH_FORMAT = "/public/dumps/public/enwiki/{date}/enwiki-{date}-pages-articles.xml.bz2"
 
     def __init__(self):
-        self.dump_file = "/public/dumps/public/enwiki/{date}/enwiki-{date}-pages-articles.xml.bz2".format(date="20170901")
+        self.dump_file = self.get_dump_file()
+        if self.dump_file is None:
+            raise RuntimeError("The dump file could not be determined.")
         self.mistakes = self.parse_config("Wikipedia:WikiProject Fix common mistakes/Scan configuration")
         self.whitelist = self.parse_whitelist("Wikipedia:WikiProject Fix common mistakes/Whitelisted pages")
         self.filter_tags = ["math", "pre", "score", "source", "syntaxhighlight"]
+
+    def get_dump_file(self):
+        wiki_folder = os.sep + os.path.join("public", "dumps", "public", "enwiki")
+        dump_dates = sorted(os.listdir(wiki_folder), reverse=True)
+        for date in dump_dates:
+            status_path = os.path.join(wiki_folder, date, "status.html")
+            with open(status_path) as status_file:
+                if "Dump complete" in status_file.read():
+                    return self.DUMP_PATH_FORMAT.format(date=date)
 
     def parse_config(self, title):
         mistakes = dict()
