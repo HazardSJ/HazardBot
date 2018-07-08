9ab4f01a8244d939ed9d669773f7c32ebad020d7
Hazard-SJ <hazardsjwiki@gmail.com>
2018-07-08 13:50:31 -0400

    Remove dynamic generation of dump file name
    It seems that there's now a `latest` directory with the latest (completed?) dump, so using that directly.

--- enwiki/common_mistakes.py
+++ enwiki/common_mistakes.py
@@ -15,24 +15,12 @@ site.login()
 class CommonMistakesLister(object):
     """Updates lists of common mistakes for WikiProject Fix common mistakes"""
     
-    DUMP_PATH_FORMAT = "/public/dumps/public/enwiki/{date}/enwiki-{date}-pages-articles.xml.bz2"
+    DUMP_FILE_NAME = "/public/dumps/public/enwiki/latest/enwiki-latest-pages-articles.xml.bz2"
+    FILTER_TAGS = ["math", "pre", "score", "source", "syntaxhighlight"]
 
     def __init__(self):
-        self.dump_file = self.get_dump_file()
-        if self.dump_file is None:
-            raise RuntimeError("The dump file could not be determined.")
         self.mistakes = self.parse_config("Wikipedia:WikiProject Fix common mistakes/Scan configuration")
         self.whitelist = self.parse_whitelist("Wikipedia:WikiProject Fix common mistakes/Whitelisted pages")
-        self.filter_tags = ["math", "pre", "score", "source", "syntaxhighlight"]
-
-    def get_dump_file(self):
-        wiki_folder = os.sep + os.path.join("public", "dumps", "public", "enwiki")
-        dump_dates = sorted(os.listdir(wiki_folder), reverse=True)
-        for date in dump_dates:
-            status_path = os.path.join(wiki_folder, date, "status.html")
-            with open(status_path) as status_file:
-                if "Dump complete" in status_file.read():
-                    return self.DUMP_PATH_FORMAT.format(date=date)
 
     def parse_config(self, title):
         mistakes = dict()
@@ -68,7 +56,7 @@ class CommonMistakesLister(object):
         text = re.sub(r"\[\[(?:File|Image):.*?(?:\|(.*?))?\]\]", lambda m: m.group(1) or "",
                       text, flags=re.DOTALL|re.IGNORECASE)
         # Strip some HTML-style tags
-        for tag in self.filter_tags:
+        for tag in self.FILTER_TAGS:
             text = re.sub(r"<%(tag)s.*?</%(tag)s>" % {"tag": tag}, "", text, flags=re.DOTALL|re.IGNORECASE)
         # And we're done here
         return text
@@ -102,7 +90,7 @@ class CommonMistakesLister(object):
                 continue
 
     def run(self):
-        dump = xmlreader.XmlDump(self.dump_file)
+        dump = xmlreader.XmlDump(self.DUMP_FILE_NAME)
         for page in dump.parse():
             if page.ns == "0" and page.title.lower() not in self.whitelist:
                 self.check_page(page)
